32b86d67c2405fe4839a39237c78f08d2ec7b386
noor <noorultracodes@gmail.com>
2023-02-16 15:32:09 +0500

    changes repace prewith loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import NavBar from "./components/NavBar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
